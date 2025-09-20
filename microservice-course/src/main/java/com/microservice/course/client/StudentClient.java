4e5c03c1120521344b15735a620a04046e08632c
Magleo Medina <xxmagleoxx.medina2003@gmail.com>
2025-09-20 14:50:18 -0400

    third commit

--- microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
+++ microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.microservice.course.dto.StudentDTO;
 
 
-@FeignClient(name="msvc-student", url = "127.0.0.1:8090/api/students")
+@FeignClient(name="msvc-student", url = "127.0.0.1:8080/api/students")
 public interface StudentClient {
 
     @GetMapping("/search-by-id/{idCourse}")
